343678e6e0939c1d68b3b868db1d01ee861e337b
$\//\/\\/ <indiesupp@gmail.com>
2022-11-29 21:36:49 +0600

    Added Hello TEST.py with function witch calculates the area of a circle by forumula: n*(r squared)
    Hello GitHub

---
+++ Hello TEST.py
@@ -0,0 +1,13 @@
+# This is test function that you can check
+# Высчитывает площадь круга по форумуле: n*(r в квадрате)
+
+
+def function():
+    r = input('введите радиус круга: ')
+    r = float(r)
+    n = 3.14
+    umn = n*(r**2)
+    print(umn)
+
+
+function()
